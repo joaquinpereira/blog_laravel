ed78d3c98fe31e48b10dddc85d2dec2247c4e2a1
Joaquin Pereira <pereira.joaquin@gmail.com>
2022-06-10 11:19:47 -0400

    edit users

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Datos Personales</h3>
+                </div>
+                <div class="box-body">
+                    @if ($errors->any())
+                        <ul class="list-group">
+                            @foreach ($errors->all() as $error)
+                                <li class="list-group-item list-group-item-danger">
+                                    {{ $error }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <form method="POST" action="{{ route('admin.users.update', $user) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div class="form-group">
+                            <label for="name">Nombre:</label>
+                            <input name="name" value="{{ old('name', $user->name) }}" type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email:</label>
+                            <input name="email" value="{{ old('email', $user->email) }}" type="text" class="form-control">
+                        </div>
+
+                        <button class="btn btn-primary btn-block">Actualizar usuario</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
